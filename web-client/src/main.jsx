20cb65b1c2ba34fe727a991a32aef1cf4b1f76d3
Great-Pittbul <greatpittbul@gmail.com>
2025-11-02 18:01:07 +0100

    Update main.jsx

--- web-client/src/main.jsx
+++ web-client/src/main.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Auth from "./Auth";
 import Chat from "./Chat";
 
+function App() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={user ? <Navigate to="/chat" /> : <Auth />} />
+        <Route path="/chat" element={user ? <Chat /> : <Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Auth />} />
-      <Route path="/chat" element={<Chat />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
